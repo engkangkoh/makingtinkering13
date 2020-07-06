618121a240940925e5295e0f9fc603115f470e4e
Engkang Koh <62113648+engkangkoh@users.noreply.github.com>
2020-07-06 18:49:14 +0800

    Add files via upload

---
+++ 1b_dist_to_center_pixel_while_loop.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: engkang
+"""
+
+import pyrealsense2 as rs                                           #importing realsense library
+
+p = rs.pipeline()                                                   #establishing connection with the realsense depth cam
+p.start()
+
+frames = p.wait_for_frames()
+depth = frames.get_depth_frame()                                    #receiving depth info
+width = depth.get_width()           
+height = depth.get_height()
+print (width, " ", height)
+dist_to_center = depth.get_distance(int(width/2), int(height/2))    #getting dist from middle pixel for testing
+print (dist_to_center)
+
+dist = getinfo()
+
+while True:
+    while dist > 1:
+        time.sleep(0.5)
+        dist = getinfo()
+
+    else:
+        #led_off()
+        time.sleep(1)
+        dist = getinfo()
+
+
+p.stop()
